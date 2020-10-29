6cfa026c69ddf5305be426365428e14c9245d4ba
Brian <37119156+BrianOni@users.noreply.github.com>
2020-10-29 18:27:08 -0400

    Add project files.

---
+++ OOPUserAuthenticationConsoleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace OOPUserAuthenticationConsoleApp
+{
+    class Program
+    {
+        static UserAccount[] arrUserAccountList = new UserAccount[10];
+
+        static void Main(string[] args)
+        {
+            //init users database
+            Initialize();
+
+            //ask about username
+            Console.WriteLine("Enter the username: ");
+            string username = Console.ReadLine();
+            //ask about password
+            Console.WriteLine("Enter the password: ");
+            string password = Console.ReadLine();
+
+            //check username and password
+            while (username != "-1" || password != "-1")
+            {
+                //try to authentificate
+                if (Authenticate(username, password))
+                    Console.WriteLine("Access has been granted");
+                else
+                    Console.WriteLine("Logon Failure. Please try again");
+
+                //ask about username
+                Console.WriteLine("\nEnter the username: ");
+                username = Console.ReadLine();
+                //ask about password
+                Console.WriteLine("Enter the password: ");
+                password = Console.ReadLine();
+            }
+
+            //end the app
+            Console.Write("Program has been terminated");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Method initialize users DB
+        /// </summary>
+        public static void Initialize()
+        {
+            arrUserAccountList[0] = new UserAccount("Joe", "111");
+            arrUserAccountList[1] = new UserAccount("Angel", "222");
+            arrUserAccountList[2] = new UserAccount("Sam", "333");
+            arrUserAccountList[3] = new UserAccount("Mary", "444");
+            arrUserAccountList[4] = new UserAccount("Nancy", "555");
+        }
+
+        /// <summary>
+        /// Method tries to authenticate user
+        /// </summary>
+        /// <param name="username">user name</param>
+        /// <param name="password">user password</param>
+        /// <returns></returns>
+        private static bool Authenticate(string username, string password)
+        {
+            //iterate by all usernames and try to find user name and password
+            for (int i = 0; i < arrUserAccountList.Length; i++)
+            {
+                if (arrUserAccountList[i] != null)
+                {
+                    if (arrUserAccountList[i].Authenticate(username, password))
+                        return true;
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
---
+++ OOPUserAuthenticationConsoleApp/UserAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OOPUserAuthenticationConsoleApp
+{
+    class UserAccount
+    {
+        Guid m_UserAccountID;
+        string m_Username;
+        string m_Password;
+        string m_Email;
+
+        public string UserAccountID { get { return m_UserAccountID.ToString(); } }
+        public string Username { get { return m_Username; } }
+        public string Password { get { return m_Password; } }
+        public string Email { get { return m_Email; } }
+
+        private UserAccount()
+        {
+            m_UserAccountID = Guid.NewGuid();
+        }
+
+        private UserAccount(string username, string password, string email): this()
+        {
+            m_Username = username;
+            m_Password = password;
+            m_Email = email;
+        }
+
+        public UserAccount(string username, string password) : this(username, password, string.Empty)
+        {
+            
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            return Username == username && Password == password;
+        }
+
+        public static UserAccount GetInstance()
+        {
+            return new UserAccount();
+        }
+
+        public static UserAccount GetInstance(string username, string password, string email)
+        {
+            return new UserAccount(username, password, email);
+        }
+
+        ~UserAccount()
+        {
+
+        }
+    }
+}
